14d7c91067373b663c91b1310ebe4c1be87f47b9
Neel <neelu.patel@mail.utoronto.ca>
2020-09-11 01:59:56 -0400

    Command handler

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 require('dotenv').config();
 const Discord = require('discord.js');
-const bot = new Discord.Client();
+const botClient = new Discord.Client();
+const botCommands = require('./commands');
+
+botClient.commands = new Discord.Collection();
+
+Object.keys(botCommands).map(key => {
+  botClient.commands.set(botCommands[key].name, botCommands[key]);
+});
 
 const TOKEN = process.env.TOKEN;
 
-bot.on('ready', () => {
-    console.info(`Logged in as ${bot.user.tag}!`);
-  });
+botClient.login(TOKEN);
 
-bot.login(TOKEN);
+botClient.on('ready', () => {
+  console.info(`Logged in as ${botClient.user.tag}!`);
+});
 
+botClient.on('message', msg => {
+  const args = msg.content.split(/ +/);
+  const command = args.shift().toLowerCase();
+  console.info(`Called command: ${command}`);
+
+  if (!botClient.commands.has(command)) return;
+
+  try {
+    botClient.commands.get(command).execute(msg, args);
+  } catch (error) {
+    console.error(error);
+    msg.reply('there was an error trying to execute that command!');
+  }
+});
